61ad6da27a89bfa6fe29feb96f077e7932ecbc46
felix mwangi <fmwauramwangi@gmail.com>
2025-01-28 10:48:01 +0300

    src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,30 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [searchText, setSearchText] = useState("");
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleSearchChange(newSearchText) {
+    setSearchText(newSearchText);
+  }
+
+  function handleItemFormSubmit(newItem) {
+    // Add the new item to the list
+    setItems([...items, newItem]);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList
+        items={items}
+        searchText={searchText}
+        onSearchChange={handleSearchChange}
+        onItemFormSubmit={handleItemFormSubmit}
+      />
     </div>
   );
 }
